129aa22c00a97e93c015cab614aea527dd2e0d96
GiangTran <secmask@gmail.com>
2023-01-06 15:09:50 +0700

    add chains

---
+++ pkg/chains/chains.go
@@ -0,0 +1,27 @@
+package chains
+
+//go:generate enumer -type=ChainID -linecomment -json=true -sql=true -yaml
+type ChainID int64
+
+const (
+	Ethereum ChainID = 1          // ethereum
+	Optimism ChainID = 10         // optimism
+	Cronos   ChainID = 25         // cronos
+	BSC      ChainID = 56         // bsc
+	ETC      ChainID = 61         // etc
+	Tomo     ChainID = 88         // tomo
+	Gnosis   ChainID = 100        // gnosis
+	Polygon  ChainID = 137        // polygon
+	BTTC     ChainID = 199        // bttc
+	Fantom   ChainID = 250        // fantom
+	KCC      ChainID = 321        // kcc
+	Moonbeam ChainID = 1284       // moonbeam
+	Kava     ChainID = 2222       // kava
+	Canto    ChainID = 7700       // canto
+	Klaytn   ChainID = 8217       // klaytn
+	Fusion   ChainID = 32659      // fusion
+	Arbitrum ChainID = 42161      // arbitrum
+	Celo     ChainID = 42220      // celo
+	Avax     ChainID = 43114      // avax
+	Aurora   ChainID = 1313161554 // aurora
+)
