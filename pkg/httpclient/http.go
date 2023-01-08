05fd78a2dcaab3c6972f2379375673a86fa9c65f
GiangTran <secmask@gmail.com>
2023-01-08 11:24:13 +0700

    update httpclient

--- pkg/httpclient/http.go
+++ pkg/httpclient/http.go
@@ -10,31 +10,33 @@ import (
 	"github.com/KyberNetwork/tradinglib/pkg/sb"
 )
 
-func DoHTTPRequest(client *http.Client, req *http.Request, out interface{}) error {
+func DoHTTPRequest(client *http.Client, req *http.Request, out interface{}, options ...Option) (*http.Response, error) {
 	if client == nil {
-		return fmt.Errorf("client must not be nil")
+		return nil, fmt.Errorf("client must not be nil")
 	}
 	if req == nil {
-		return fmt.Errorf("req must not be nil")
+		return nil, fmt.Errorf("req must not be nil")
 	}
+	option := defaultOption()
+	option.apply(options...)
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	data, err := io.ReadAll(resp.Body)
 	_ = resp.Body.Close()
 	if err != nil {
-		return fmt.Errorf("read respsonse error %w", err)
+		return nil, fmt.Errorf("read respsonse error %w", err)
 	}
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected code %d: %s", resp.StatusCode, data)
+	if resp.StatusCode != option.expectedStatusCode {
+		return nil, fmt.Errorf("unexpected code %d: %s", resp.StatusCode, data)
 	}
 	if out != nil {
 		if err = json.Unmarshal(data, out); err != nil {
-			return fmt.Errorf("unmarshal error: %w - %s", err, data)
+			return nil, fmt.Errorf("unmarshal error: %w - %s", err, data)
 		}
 	}
-	return nil
+	return resp, nil
 }
 
 func NewRequest(method, baseURL, path string, query Query, body io.Reader) (*http.Request, error) {
@@ -45,12 +47,15 @@ func NewRequest(method, baseURL, path string, query Query, body io.Reader) (*htt
 	return http.NewRequest(method, url, body)
 }
 
-func NewPostJSONWithReader(baseURL, path string, query Query, body io.Reader) (*http.Request, error) {
+func NewGet(baseURL, path string, query Query) (*http.Request, error) {
+	return NewRequest(http.MethodGet, baseURL, path, query, nil)
+}
+
+func NewPost(baseURL, path string, query Query, body io.Reader) (*http.Request, error) {
 	req, err := NewRequest(http.MethodPost, baseURL, path, query, body)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Content-Type", "application/json")
 	return req, nil
 }
 
@@ -63,5 +68,10 @@ func NewPostJSON(baseURL, path string, query Query, body interface{}) (*http.Req
 		}
 		buff = bytes.NewBuffer(data)
 	}
-	return NewPostJSONWithReader(baseURL, path, query, buff)
+	out, err := NewPost(baseURL, path, query, buff)
+	if err != nil {
+		return nil, err
+	}
+	out.Header.Set("Content-Type", "application/json")
+	return out, nil
 }
