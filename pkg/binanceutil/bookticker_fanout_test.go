e522003904249bb90ce7108d8b78c43f0aeb69ba
thanhpp <thanhphanphu18@gmail.com>
2023-03-01 17:46:33 +0700

    test: ✅ binanceutil: add ws test
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- pkg/binanceutil/bookticker_fanout_test.go
+++ pkg/binanceutil/bookticker_fanout_test.go
@@ -4,23 +4,43 @@ import (
 	"testing"
 	"time"
 
-	"github.com/KyberNetwork/tradinglib/pkg/binanceutil"
+	"github.com/adshao/go-binance/v2"
+	"github.com/stretchr/testify/require"
 )
 
-func TestBookTickerFanout(t *testing.T) {
-	var (
-		symbols = []string{"KNCUSDT", "LINKUSDT", "C98USDT"}
-		tick    = time.Tick(time.Second * 5)
-	)
+// func TestBookTickerFanout(t *testing.T) {
+// 	var (
+// 		symbols = []string{"KNCUSDT", "LINKUSDT", "C98USDT"}
+// 		tick    = time.Tick(time.Second * 5)
+// 	)
 
-	bt := binanceutil.NewBookTickerFanOut(10, 10)
-	for i := range symbols {
-		go func(idx int) {
-			c := bt.Subscribe(symbols[idx])
-			for e := range c {
-				t.Log(e)
-			}
-		}(i)
-	}
+// 	bt := binanceutil.NewBookTickerFanOut(10, 10)
+// 	for i := range symbols {
+// 		go func(idx int) {
+// 			c := bt.Subscribe(symbols[idx])
+// 			for e := range c {
+// 				t.Log(e)
+// 			}
+// 		}(i)
+// 	}
+// 	<-tick
+// }
+
+func TestWs(t *testing.T) {
+	tick := time.Tick(time.Second * 5)
+	symbols := []string{"KNCUSDT", "LINKUSDT", "C98USDT"}
+	go func() {
+		done, _, err := binance.WsCombinedMarketStatServe(
+			symbols,
+			func(event *binance.WsMarketStatEvent) {
+				t.Log(event)
+			},
+			func(err error) {
+				panic("subscribe error")
+			})
+		require.NoError(t, err)
+		<-done
+	}()
+
 	<-tick
 }
