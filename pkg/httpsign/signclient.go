af693e430fdb9744ce24d0857ed0ebbb64bca244
nguyenkhanhviet <viet@kyber.network>
2023-04-22 14:39:49 +0700

    add timeout for client

--- pkg/httpsign/signclient.go
+++ pkg/httpsign/signclient.go
@@ -2,11 +2,20 @@ package httpsign
 
 import (
 	"net/http"
+	"time"
 )
 
+// becareful, this client doesn't have timeout.
 func NewClient(transport http.RoundTripper, key string, secret []byte) *http.Client {
 	return &http.Client{
 		Transport: NewTransport(transport, key, secret),
+	}
+}
+
+func NewClientWithOptions(transport http.RoundTripper, key string, secret []byte, timeout time.Duration) *http.Client {
+	return &http.Client{
+		Transport: NewTransport(transport, key, secret),
+		Timeout:   timeout,
 	}
 }
 
