9e87554ae1b53e7c199f925bce92b911766e96e5
thanhpp <thanhphanphu18@gmail.com>
2024-03-08 13:37:13 +0700

    feat(prod): ✨ pkg: hashset
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

---
+++ pkg/hashset/hashset.go
@@ -0,0 +1,35 @@
+package hashset
+
+import "golang.org/x/exp/maps"
+
+type HashSet[K comparable] struct {
+	m map[K]struct{}
+}
+
+func New[K comparable]() *HashSet[K] {
+	return &HashSet[K]{
+		m: map[K]struct{}{},
+	}
+}
+
+func (h *HashSet[K]) Contains(k K) bool {
+	_, ok := h.m[k]
+
+	return ok
+}
+
+func (h *HashSet[K]) Add(k K) {
+	h.m[k] = struct{}{}
+}
+
+func (h *HashSet[K]) Remove(k K) {
+	delete(h.m, k)
+}
+
+func (h *HashSet[K]) Size() int {
+	return len(h.m)
+}
+
+func (h *HashSet[K]) Clear() {
+	maps.Clear(h.m)
+}
---
+++ pkg/hashset/hashset_sync.go
@@ -0,0 +1,62 @@
+package hashset
+
+import "sync"
+
+type SyncHashSet[K comparable] struct {
+	rw sync.RWMutex
+	s  *HashSet[K]
+}
+
+func NewSync[K comparable]() *SyncHashSet[K] {
+	return &SyncHashSet[K]{
+		rw: sync.RWMutex{},
+		s:  New[K](),
+	}
+}
+
+func (sh *SyncHashSet[K]) Contains(k K) bool {
+	sh.rw.RLock()
+	defer sh.rw.RUnlock()
+
+	return sh.s.Contains(k)
+}
+
+func (sh *SyncHashSet[K]) ContainsOrAdd(k K) (isContains bool) {
+	sh.rw.Lock()
+	defer sh.rw.Unlock()
+	if sh.s.Contains(k) {
+		return true
+	}
+
+	sh.s.Add(k)
+
+	return false
+}
+
+func (sh *SyncHashSet[K]) Add(k K) {
+	sh.rw.Lock()
+	defer sh.rw.Unlock()
+
+	sh.s.Add(k)
+}
+
+func (sh *SyncHashSet[K]) Remove(k K) {
+	sh.rw.Lock()
+	defer sh.rw.Unlock()
+
+	sh.s.Remove(k)
+}
+
+func (sh *SyncHashSet[K]) Size() int {
+	sh.rw.RLock()
+	defer sh.rw.RUnlock()
+
+	return sh.s.Size()
+}
+
+func (sh *SyncHashSet[K]) Clear() {
+	sh.rw.Lock()
+	defer sh.rw.Unlock()
+
+	sh.s.Clear()
+}
