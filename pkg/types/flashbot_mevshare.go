2ef42b808946fa3616d46de6063a61976e97d8bc
(Nam) Pham Tien <phamtiennam2301@gmail.com>
2024-04-24 15:31:55 +0700

    feat: update Flashbot Mevshare struct

--- pkg/types/flashbot_mevshare.go
+++ pkg/types/flashbot_mevshare.go
@@ -5,9 +5,26 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
-type FlashbotMevshareTxHint struct {
+type FlashbotMevshareLog struct {
+	// address of the contract that generated the event
+	Address common.Address `json:"address"`
+	// list of topics provided by the contract.
+	Topics []common.Hash `json:"topics"`
+	// supplied by the contract, usually ABI-encoded
+	Data hexutil.Bytes `json:"data"`
+}
+
+type FlashbotMevShareTxHint struct {
 	Hash             *common.Hash    `json:"hash,omitempty"`
 	To               *common.Address `json:"to,omitempty"`
 	FunctionSelector *hexutil.Bytes  `json:"functionSelector,omitempty"`
 	CallData         *hexutil.Bytes  `json:"callData,omitempty"`
+}
+
+type FlashbotMevshareEvent struct {
+	Hash        common.Hash              `json:"hash"`
+	Logs        []FlashbotMevshareLog    `json:"logs"`
+	Txs         []FlashbotMevShareTxHint `json:"txs"`
+	MevGasPrice *hexutil.Big             `json:"mevGasPrice,omitempty"`
+	GasUsed     *hexutil.Uint64          `json:"gasUsed,omitempty"`
 }
