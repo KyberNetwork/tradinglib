352c42e1da527d6b050e1523cd46cfffb1a20dd1
Dat Luong <dat.luong@kyber.network>
2024-06-20 11:25:32 +0700

    feat: implement logic for mevblocker

---
+++ pkg/mev/backrun_public_sender_test.go
@@ -0,0 +1,39 @@
+package mev
+
+import (
+	"testing"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"context"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"time"
+)
+
+func TestNewBackrunPublicClient(t *testing.T) {
+	t.Skip()
+
+	ethClient, err := ethclient.Dial("https://ethereum-rpc.publicnode.com")
+	require.NoError(t, err)
+
+	blockNumber, err := ethClient.BlockNumber(context.Background())
+	require.NoError(t, err)
+	t.Log("blockNumber", blockNumber)
+
+	// Transaction hashes you want to fetch from the node
+	txHashes := []string{
+		"0x2e038916d175d9028c87d59e33f79ac96cb487e90aad6cd501dc9675b64d7245",
+	}
+	tx, isPending, err := ethClient.TransactionByHash(context.Background(), common.HexToHash(txHashes[0]))
+	require.NoError(t, err)
+	require.False(t, isPending)
+
+	httpCl := http.Client{Timeout: time.Second * 5}
+	// Initialize the client
+	senderClient := NewBackrunPublicClient(&httpCl, "https://rpc.mevblocker.io", nil, BundleSenderTypeMevBlocker)
+
+	pendingTXhash := common.HexToHash("0x79d48b1a25d7af0d815997d2ce3a127560080971c5ea98ca5a32424f604e09fb")
+	resp, err := senderClient.SendBackrunBundle(context.Background(), nil, blockNumber, pendingTXhash, []string{}, tx)
+	t.Log("resp", resp)
+	t.Log("err", err)
+}
