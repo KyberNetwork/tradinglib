4f62e6810b1afe1a25251456ab3d2efcd9d2b1c2
Chien Nguyen <chien.nguyen@kyber.network>
2024-08-16 15:47:08 +0700

    Use math.PaddedBigBytes instead

--- pkg/oneinch/fusionorder/encode.go
+++ pkg/oneinch/fusionorder/encode.go
@@ -1,22 +1,11 @@
 package fusionorder
 
-import "math/big"
+import (
+	"math/big"
 
-// padOrTrim returns (size) bytes from input (bb)
-// Short bb gets zeros prefixed, Long bb gets left/MSB bits trimmed.
-func padOrTrim(bb []byte, size int) []byte {
-	l := len(bb)
-	if l == size {
-		return bb
-	}
-	if l > size {
-		return bb[l-size:]
-	}
-	tmp := make([]byte, size)
-	copy(tmp[size-l:], bb)
-	return tmp
-}
+	"github.com/ethereum/go-ethereum/common/math"
+)
 
 func encodeInt64ToBytes(n int64, size int) []byte {
-	return padOrTrim(big.NewInt(n).Bytes(), size)
+	return math.PaddedBigBytes(big.NewInt(n), size)
 }
