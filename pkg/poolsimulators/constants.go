5e510208f1689c2ced927d494dcd2b88d909b64b
Dat Luong <dat.luong@kyber.network>
2024-11-21 10:49:14 +0700

    feat: add pool factory for ipools and add new dexes

---
+++ pkg/poolsimulators/constants.go
@@ -0,0 +1,19 @@
+package poolsimulators
+
+import (
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
+)
+
+// DefaultGasAlgebra
+// nolint: gochecknoglobals
+var (
+	DefaultGasAlgebra = map[valueobject.Exchange]int64{
+		valueobject.ExchangeQuickSwapV3: 280000,
+		valueobject.ExchangeSynthSwapV3: 280000,
+		valueobject.ExchangeSwapBasedV3: 280000,
+		valueobject.ExchangeLynex:       280000,
+		valueobject.ExchangeCamelotV3:   280000,
+		valueobject.ExchangeZyberSwapV3: 280000,
+		valueobject.ExchangeThenaFusion: 280000,
+	}
+)
