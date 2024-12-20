a78401ada64cd8c68eb0d836de71ff175bd7ac70
Dat Luong <dat.luong@kyber.network>
2024-12-20 17:42:59 +0700

    feat: add id col

--- pkg/types/bundles.go
+++ pkg/types/bundles.go
@@ -7,6 +7,7 @@ import (
 )
 
 type ExecuteBundle struct {
+	ID          uint64         `db:"id"`
 	TxHashes    pq.StringArray `db:"tx_hashes"` // Use pq.StringArray instead of []string
 	BundleHash  string         `db:"bundle_hash"`
 	BlockNumber uint64         `db:"block_number"`
