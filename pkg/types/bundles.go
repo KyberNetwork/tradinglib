63982506eb22ea55ea95f602ebc259c77f6f8df8
Dat Luong <105619094+datluongductuan@users.noreply.github.com>
2024-12-23 14:25:20 +0700

    feat: add bundles type (#108)
    * feat: add bundles type
    
    * feat: add id col
    
    * fix type
    
    * add sender column

---
+++ pkg/types/bundles.go
@@ -0,0 +1,17 @@
+package types
+
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
+
+type ExecuteBundle struct {
+	ID          int64          `db:"id"`
+	TxHashes    pq.StringArray `db:"tx_hashes"` // Use pq.StringArray instead of []string
+	BundleHash  string         `db:"bundle_hash"`
+	BlockNumber uint64         `db:"block_number"`
+	SubmitAt    time.Time      `db:"submit_at"`
+	BuilderName string         `db:"builder_name"`
+	Sender      string         `db:"sender"`
+}
