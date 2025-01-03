05e474f176d895d87c527d81f7bf15241e434dfa
Dat Luong <dat.luong@kyber.network>
2025-01-03 10:09:27 +0700

    feat: add col arb type

--- pkg/types/bundles.go
+++ pkg/types/bundles.go
@@ -7,11 +7,12 @@ import (
 )
 
 type ExecuteBundle struct {
-	ID          int64          `db:"id"`
-	TxHashes    pq.StringArray `db:"tx_hashes"` // Use pq.StringArray instead of []string
-	BundleHash  string         `db:"bundle_hash"`
-	BlockNumber uint64         `db:"block_number"`
-	SubmitAt    time.Time      `db:"submit_at"`
-	BuilderName string         `db:"builder_name"`
-	Sender      string         `db:"sender"`
+	ID            int64          `db:"id"`
+	TxHashes      pq.StringArray `db:"tx_hashes"` // Use pq.StringArray instead of []string
+	BundleHash    string         `db:"bundle_hash"`
+	BlockNumber   uint64         `db:"block_number"`
+	SubmitAt      time.Time      `db:"submit_at"`
+	BuilderName   string         `db:"builder_name"`
+	Sender        string         `db:"sender"`
+	ArbitrageType string         `db:"arb_type"`
 }
