3441c04b79e1de52aaabf69ea57d5401ea7e74a3
hiepnv90 <87631229+hiepnv90@users.noreply.github.com>
2025-04-25 14:15:33 +0700

    Update kyberswap-dex-lib (#134)

--- pkg/poolsimulators/poolsimulators.go
+++ pkg/poolsimulators/poolsimulators.go
@@ -35,9 +35,10 @@ func newSwapLimit(dex string, limit map[string]*big.Int) pkgpool.SwapLimit {
 		pooltypes.PoolTypes.NativeV1,
 		pooltypes.PoolTypes.Dexalot,
 		pooltypes.PoolTypes.RingSwap,
-		pooltypes.PoolTypes.MxTrading,
 		pooltypes.PoolTypes.LO1inch,
-		pooltypes.PoolTypes.KyberPMM:
+		pooltypes.PoolTypes.KyberPMM,
+		pooltypes.PoolTypes.Pmm1,
+		pooltypes.PoolTypes.Pmm2:
 		return swaplimit.NewInventory(dex, limit)
 
 	case pooltypes.PoolTypes.LimitOrder:
