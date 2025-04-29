29172bb3da90b7aa49bf0a013f8f129edd249e8e
Dat Luong <105619094+datluongductuan@users.noreply.github.com>
2025-04-29 14:35:11 +0700

    update new CalcNextBaseFee logic (#135)

--- pkg/eth/base_fee.go
+++ pkg/eth/base_fee.go
@@ -4,26 +4,29 @@ import (
 	"math/big"
 )
 
-// [STEAL] https://github.com/KyberNetwork/1inch-resolver/blob/v0.2.13/internal/gas/tracker.go#L157
-func CalcNextBaseFee(targetGas, baseFeeChangeDenominator, gasUsed uint64, baseFee *big.Int) *big.Int {
-	if gasUsed == targetGas {
-		return new(big.Int).Set(baseFee)
+const (
+	defaultTargetDenominator        = 2
+	defaultBaseFeeChangeDenominator = 8
+)
+
+// [STEAL] https://github.com/KyberNetwork/1inch-resolver/blob/4bac2bf061c311ad297967dafc7b881a9191fba9/internal/gas/tracker.go#L160C21-L179C2
+func CalcNextBaseFee(parentBaseFee *big.Int, gasUsed uint64, gasLimit uint64) *big.Int {
+	defaultTargetGas := gasLimit / defaultTargetDenominator
+	if gasUsed == defaultTargetGas {
+		return parentBaseFee
 	}
 
-	prodTargetGasAndDenominator := new(big.Int).SetUint64(targetGas * baseFeeChangeDenominator)
-
-	if gasUsed > targetGas {
+	if gasUsed > defaultTargetGas {
 		// delta = baseFee * (gasUsed - target) / (target * baseFeeChangeDenominator)
-		delta := new(big.Int).SetUint64(gasUsed - targetGas)
-		delta.Mul(baseFee, delta)
-		delta.Div(delta, prodTargetGasAndDenominator)
-		return delta.Add(baseFee, delta)
+		delta := new(big.Int).SetUint64(gasUsed - defaultTargetGas)
+		delta.Mul(parentBaseFee, delta)
+		delta.Div(delta, new(big.Int).SetUint64(defaultTargetGas*defaultBaseFeeChangeDenominator))
+		return delta.Add(parentBaseFee, delta)
 	}
 
 	// delta = baseFee * (target - gasUsed) / (target * baseFeeChangeDenominator)
-	delta := new(big.Int).SetUint64(targetGas - gasUsed)
-	delta.Mul(baseFee, delta)
-	delta.Div(delta, prodTargetGasAndDenominator)
-
-	return delta.Sub(baseFee, delta)
+	delta := new(big.Int).SetUint64(defaultTargetGas - gasUsed)
+	delta.Mul(parentBaseFee, delta)
+	delta.Div(delta, new(big.Int).SetUint64(defaultTargetGas*defaultBaseFeeChangeDenominator))
+	return delta.Sub(parentBaseFee, delta)
 }
