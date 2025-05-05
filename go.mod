efe07964fff7f5eabdf6036f34cd1f7e21e29717
thanhpp <thanhphanphu18@gmail.com>
2025-05-05 11:47:01 +0700

    refactor: ♻️ restore poolsimulators
    Signed-off-by: thanhpp <thanhphanphu18@gmail.com>

--- go.mod
+++ go.mod
@@ -129,13 +129,11 @@ require (
 	golang.org/x/arch v0.14.0 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/exp v0.0.0-20250218142911-aa4b98e5adaa // indirect
-	golang.org/x/mod v0.24.0 // indirect
 	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 	golang.org/x/time v0.10.0 // indirect
-	golang.org/x/tools v0.32.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250204164813-702378808489 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250204164813-702378808489 // indirect
 	google.golang.org/grpc v1.70.0 // indirect
--- pkg/poolsimulators/poolsimulators.go
+++ pkg/poolsimulators/poolsimulators.go
@@ -1,57 +1,74 @@
 package poolsimulators
 
+import (
+	"errors"
+	"fmt"
+	"math/big"
+
+	ksent "github.com/KyberNetwork/kyberswap-dex-lib/pkg/entity"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/pooltypes"
+	pkgpool "github.com/KyberNetwork/kyberswap-dex-lib/pkg/source/pool"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/swaplimit"
+	"github.com/KyberNetwork/kyberswap-dex-lib/pkg/valueobject"
+)
+
 // DEPRECATED: pls refer to https://github.com/KyberNetwork/reserve-taker/blob/v4.0.4/internal/pool/parse_pool.go#L10-L23
 
-// var ErrPoolTypeNotSupported = errors.New("pool type is not supported")
+var ErrPoolTypeNotSupported = errors.New("pool type is not supported")
 
-// // PoolSimulatorFromPool init pool sim.
-// func PoolSimulatorFromPool(pool ksent.Pool, chainID uint) (pkgpool.IPoolSimulator, error) {
-// 	factoryFn := pkgpool.Factory(pool.Type)
-// 	if factoryFn == nil {
-// 		return nil, fmt.Errorf("%w: %s", ErrPoolTypeNotSupported, pool.Type)
-// 	}
+// PoolSimulatorFromPool init pool sim.
+func PoolSimulatorFromPool(pool ksent.Pool, chainID uint) (pkgpool.IPoolSimulator, error) {
+	factoryFn := pkgpool.Factory(pool.Type)
+	if factoryFn == nil {
+		return nil, fmt.Errorf("%w: %s", ErrPoolTypeNotSupported, pool.Type)
+	}
 
-// 	return factoryFn(pkgpool.FactoryParams{
-// 		EntityPool:  pool,
-// 		ChainID:     valueobject.ChainID(chainID),
-// 		BasePoolMap: nil,
-// 		EthClient:   nil,
-// 	})
-// }
+	return factoryFn(pkgpool.FactoryParams{
+		EntityPool:  pool,
+		ChainID:     valueobject.ChainID(chainID),
+		BasePoolMap: nil,
+		EthClient:   nil,
+	})
+}
 
-// func newSwapLimit(dex string, limit map[string]*big.Int) pkgpool.SwapLimit {
-// 	switch dex {
-// 	case pooltypes.PoolTypes.Synthetix,
-// 		pooltypes.PoolTypes.NativeV1,
-// 		pooltypes.PoolTypes.Dexalot,
-// 		pooltypes.PoolTypes.RingSwap,
-// 		pooltypes.PoolTypes.LO1inch,
-// 		pooltypes.PoolTypes.KyberPMM,
-// 		pooltypes.PoolTypes.Pmm1,
-// 		pooltypes.PoolTypes.Pmm2:
-// 		return swaplimit.NewInventory(dex, limit)
+// https://github.com/KyberNetwork/router-service/blob/v1.98.4/internal/pkg/usecase/poolfactory/pool_factory.go#L189C1-L214C2
+func newSwapLimit(
+	dex string,
+	limit map[string]*big.Int,
+	// poolManagerExtraData usecasetypes.PoolManagerExtraData,
+) pkgpool.SwapLimit {
+	switch dex {
+	case pooltypes.PoolTypes.Synthetix,
+		pooltypes.PoolTypes.NativeV1,
+		pooltypes.PoolTypes.Dexalot,
+		pooltypes.PoolTypes.RingSwap,
+		pooltypes.PoolTypes.LO1inch,
+		pooltypes.PoolTypes.KyberPMM,
+		pooltypes.PoolTypes.Pmm1,
+		pooltypes.PoolTypes.Pmm2:
+		return swaplimit.NewInventory(dex, limit)
 
-// 	case pooltypes.PoolTypes.LimitOrder:
-// 		return swaplimit.NewInventoryWithAllowedSenders(
-// 			dex,
-// 			limit,
-// 			// here just for usecase of kyberswap only, there are some client have priority private limit orders.
-// 			"",
-// 		)
+	case pooltypes.PoolTypes.LimitOrder:
+		return swaplimit.NewInventoryWithAllowedSenders(
+			dex,
+			limit,
+			// here just for usecase of kyberswap only, there are some client have priority private limit orders.
+			"",
+		)
 
-// 	case pooltypes.PoolTypes.Bebop:
-// 		return swaplimit.NewSingleSwapLimit(dex)
-// 	}
+	case pooltypes.PoolTypes.Bebop:
+		return swaplimit.NewSingleSwapLimit(dex)
+	}
 
-// 	return nil
-// }
+	return nil
+}
 
-// func NewSwapLimit(limits map[string]map[string]*big.Int) map[string]pkgpool.SwapLimit {
-// 	limitMap := make(map[string]pkgpool.SwapLimit, len(limits))
+func NewSwapLimit(limits map[string]map[string]*big.Int) map[string]pkgpool.SwapLimit {
+	limitMap := make(map[string]pkgpool.SwapLimit, len(limits))
 
-// 	for dex, limit := range limits {
-// 		limitMap[dex] = newSwapLimit(dex, limit)
-// 	}
+	for dex, limit := range limits {
+		limitMap[dex] = newSwapLimit(dex, limit)
+	}
 
-// 	return limitMap
-// }
+	return limitMap
+}
