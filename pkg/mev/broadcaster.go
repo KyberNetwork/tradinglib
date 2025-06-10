6900059f29dcc4a75641f79bad2f0f5059bb399d
hai pham <minhhaipham8899@gmail.com>
2025-06-10 10:03:22 +0700

    feat: add bundle rpc

---
+++ pkg/mev/broadcaster.go
@@ -0,0 +1,29 @@
+package mev
+
+const (
+	EndpointFlashbot    = "https://relay.flashbots.net"
+	EndpointBloxroute   = "https://mev.api.blxrbdn.com"
+	EndpointBeaverbuild = "https://rpc.beaverbuild.org"
+	EndpointRsync       = "https://rsync-builder.xyz"
+	EndpointTitan       = "http://ap.rpc.titanbuilder.xyz"
+	EndpointMevBlocker  = "https://rpc.mevblocker.io"
+	EndpointJetbldr     = "https://rpc.jetbldr.xyz"
+	EndpointLoki        = "https://rpc.lokibuilder.xyz"
+	EndpointMerkle      = "https://mempool.merkle.io/relay"
+	EndpointQuasar      = "https://rpc.quasar.win"
+	EndpointBuilderNet  = "https://direct-us.buildernet.org"
+)
+
+const (
+	BuilderFlashbotID    string = "builder-flashbot"
+	BuilderBloxrouteID   string = "builder-bloxroute"
+	BuilderBeaverbuildID string = "builder-beaverbuild"
+	BuilderRsyncID       string = "builder-rsync"
+	BuilderTitanID       string = "builder-titan"
+	BuilderMevBlockerID  string = "builder-mev_blocker"
+	BuilderPenguinID     string = "builder-penguin"
+	BuilderJetbldrID     string = "builder-jetbldr"
+	BuilderLokiID        string = "builder-loki"
+	BuilderMerkleID      string = "builder-merkle"
+	BuilderBlinkID       string = "builder-blink"
+)
